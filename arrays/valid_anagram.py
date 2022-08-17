925a93863d07144b478fb740bc711d14d9387fdf
rishabhpreethan <rishabh.preethan@gmail.com>
2022-08-17 16:46:55 +0530

    committed

---
+++ arrays/valid_anagram.py
@@ -0,0 +1,22 @@
+d1={}
+d2={}
+s='anagram'
+t='nagaram'
+
+#creating dictionaries with count of letters of string 1
+for i in s:
+    d1[i]=0
+for i in s:
+    d1[i]+=1
+
+#for string 2
+for i in t:
+    d2[i]=0
+for i in t:
+    d2[i]+=1
+
+#prints true if both dictionaries are equal
+if d1==d2:
+    print(True)
+else:
+    print(False)

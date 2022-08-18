a61c8f5bdbd8b3c86609f5d28464b0fbf39ef429
rishabhpreethan <rishabh.preethan@gmail.com>
2022-08-18 14:53:36 +0530

    medium

--- leetcode_daily/reduce_array_size_to_the_half.py
+++ leetcode_daily/reduce_array_size_to_the_half.py
@@ -3,6 +3,7 @@
 def minSetSize(arr):
     d = {}
     
+    #making dictionary with number as key and its count as the value
     for i in arr:
         if i in d:
             d[i]+=1
@@ -11,11 +12,12 @@ def minSetSize(arr):
     dv = list(d.values())
     ct = 0
     out = 0
+
+    #sorting it in descending order
     dv.sort(reverse = True)
     for i in dv:
         ct+=i
         out+=1
-        print(ct,out)
         if ct>=len(arr)//2:
             return out
     

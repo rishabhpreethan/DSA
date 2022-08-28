85c175c82421571bf7b78602e32ba7253cbc0bbd
rishabhpreethan <rishabh.preethan@gmail.com>
2022-08-28 19:32:18 +0530

    committed

--- linked_list/linked_list_cycle.py
+++ linked_list/linked_list_cycle.py
@@ -1,6 +1,6 @@
 #EASY
 
-
+# 141
 # Given head, the head of a linked list, determine if the linked list has a cycle in it.
 # There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 # Return true if there is a cycle in the linked list. Otherwise, return false.
@@ -27,12 +27,20 @@
 
 
 def hasCycle(self, head):
+    #slow linked list and fast linked list start at the same node
     slow=head
     fast=head
     
     while fast and fast.next:
+        
+        #slow moves one node at a time  
         slow=slow.next
+
+        #fast moves two nodes at a time
         fast=fast.next.next
+
+        #since there is a loop it is bound to be equal at some point 
         if slow==fast:
             return True
+            
     return False

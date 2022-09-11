43226cc6d46e24dd11865c0b26e0f7cf28621184
rishabhpreethan <rishabh.preethan@gmail.com>
2022-09-11 13:54:08 +0530

    commtted

--- leetcode_daily/maximum_performance_of_a_team.py
+++ leetcode_daily/maximum_performance_of_a_team.py
@@ -1,6 +1,11 @@
 #HARD
 
 
+'''     Refer    '''
+''' https://youtu.be/Y7UTvogADH0 '''
+
+
+# 1383
 # You are given two integers n and k and two integer arrays speed and efficiency both of length n. There are n engineers numbered from 1 to n. speed[i] and efficiency[i] represent the speed and efficiency of the ith engineer respectively.
 # Choose at most k different engineers out of the n engineers to form a team with the maximum performance.
 # The performance of a team is the sum of their engineers' speeds multiplied by the minimum efficiency among their engineers.

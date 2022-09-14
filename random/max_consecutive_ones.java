597226bbd8667e940aa00f9c1a100067136c7af1
rishabhpreethan <rishabh.preethan@gmail.com>
2022-09-14 21:06:02 +0530

    committed

--- random/max_consecutive_ones.java
+++ random/max_consecutive_ones.java
@@ -1,4 +1,19 @@
-import java.util.*;
+//EASY
+
+//485
+//Given a binary array nums, return the maximum number of consecutive 1's in the array.
+//
+//Example 1:
+//Input: nums = [1,1,0,1,1,1]
+//Output: 3
+////---------------------------------------------------
+//Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+//
+//Example 2:
+//Input: nums = [1,0,1,1,0,1]
+//Output: 2
+
+
 
 public class max_consecutive_ones {
     public static int findMaxConsecutiveOnes(int[] nums) {

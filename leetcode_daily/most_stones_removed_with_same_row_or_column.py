ddd036c02d89cb6e03df261300cc95f67e4baca8
rishabhpreethan <rishabh.preethan@gmail.com>
2022-11-14 21:14:54 +0530

    committed

--- leetcode_daily/most_stones_removed_with_same_row_or_column.py
+++ leetcode_daily/most_stones_removed_with_same_row_or_column.py
@@ -1,6 +1,8 @@
 #MEDIUM
 #947
 
+'''     https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/      '''
+
 # On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
 # A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
 # Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, return the largest possible number of stones that can be removed.

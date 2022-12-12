bc85da496f130e328865e05e8117c8e2e1c38e69
rishabhpreethan <rishabh.preethan@gmail.com>
2022-12-12 09:01:51 +0530

    committed

---
+++ recursion/climbing_stairs.py
@@ -0,0 +1,34 @@
+#EASY
+
+# 70
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+# Example 1:
+# Input: n = 2
+# Output: 2
+# ---------
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+# Example 2:
+# Input: n = 3
+# Output: 3
+# ---------
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+
+
+def climbStairs(n):        
+    o,t=1,1
+    
+    for i in range(n-1):
+        temp=o
+        o=o+t
+        t=temp
+    return o
+
+print(climbStairs(3))

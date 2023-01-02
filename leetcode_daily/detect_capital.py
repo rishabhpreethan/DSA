7063b5c757e10d811bb3401dc08a3f50e721e83d
rishabhpreethan <rishabh.preethan@gmail.com>
2023-01-02 14:03:38 +0530

    committed

---
+++ leetcode_daily/detect_capital.py
@@ -0,0 +1,25 @@
+#EASY
+# 520
+
+'''     https://leetcode.com/problems/detect-capital/description/       '''
+
+# We define the usage of capitals in a word to be right when one of the following cases holds:
+# All letters in this word are capitals, like "USA".
+# All letters in this word are not capitals, like "leetcode".
+# Only the first letter in this word is capital, like "Google".
+# Given a string word, return true if the usage of capitals in it is right.
+
+
+# Example 1:
+# Input: word = "USA"
+# Output: true
+
+# Example 2:
+# Input: word = "FlaG"
+# Output: false
+
+
+def detectCapitalUse(word):
+    return word.isupper() or word.islower() or word.istitle()
+
+print(detectCapitalUse("USA"))

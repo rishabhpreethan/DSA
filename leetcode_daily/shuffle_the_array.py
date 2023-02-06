6bb3deecde2a1fda8d51a17ed68cc23802f85043
rishabhpreethan <rishabh.preethan@gmail.com>
2023-02-06 08:28:17 +0530

    committed

---
+++ leetcode_daily/shuffle_the_array.py
@@ -0,0 +1,38 @@
+#EASY
+# 1470
+
+'''     https://leetcode.com/problems/shuffle-the-array/description/        '''
+
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+
+# Example 1:
+# Input: nums = [2,5,1,3,4,7], n = 3
+# Output: [2,3,5,4,1,7] 
+
+# Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+# Example 2:
+# Input: nums = [1,2,3,4,4,3,2,1], n = 4
+# Output: [1,4,2,3,3,2,4,1]
+
+# Example 3:
+# Input: nums = [1,1,2,2], n = 2
+# Output: [1,2,1,2]
+
+
+def shuffle(nums,n):
+    nl=[]
+    l=len(nums)
+    mid=int(l/2)
+    n1=nums[0:mid]
+    n2=nums[mid:]
+    # print(n1,n2)
+    le=len(n1)
+    for i in range(le):
+        nl.append(n1[i])
+        nl.append(n2[i])
+    return nl
+
+print(shuffle([2,5,1,3,4,7],3))

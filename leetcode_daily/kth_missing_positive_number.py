1e8a2bfa0a18cfd389977e901b341e507b1c274c
rishabhpreethan <rishabh.preethan@gmail.com>
2023-03-06 16:55:46 +0530

    committed

---
+++ leetcode_daily/kth_missing_positive_number.py
@@ -0,0 +1,32 @@
+#EASY
+# 1539
+
+'''     https://leetcode.com/problems/kth-missing-positive-number/      '''
+
+# Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+# Return the kth positive integer that is missing from this array.
+
+
+# Example 1:
+# Input: arr = [2,3,4,7,11], k = 5
+# Output: 9
+# --------------------------------------------------------------
+# Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+# Example 2:
+# Input: arr = [1,2,3,4], k = 2
+# Output: 6
+# --------------------------------------------------------------
+# Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+
+
+def findKthPositive(arr,k):
+    currnum=0
+    while True:
+        currnum+=1
+        if currnum not in arr:
+            k-=1
+        if k<1:
+            return currnum
+        
+print(findKthPositive([2,3,4,7,11],5))

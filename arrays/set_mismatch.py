dc2d47c91436aac4942ddfbe5dfcd5a3e97557e6
rishabhpreethan <rishabh.preethan@gmail.com>
2023-03-13 08:19:03 +0530

    committed

---
+++ arrays/set_mismatch.py
@@ -0,0 +1,26 @@
+#EASY
+# 645
+
+'''     https://leetcode.com/problems/set-mismatch/description/     '''
+
+# You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+# You are given an integer array nums representing the data status of this set after the error.
+# Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+
+# Example 1:
+# Input: nums = [1,2,2,4]
+# Output: [2,3]
+
+# Example 2:
+# Input: nums = [1,1]
+# Output: [1,2]
+
+
+def findErrorNums(nums):
+    setdiff=sum(nums)-sum(set(nums))
+    n=len(nums)
+    missing=setdiff+((n*(n+1))//2)-sum(nums)
+    return [setdiff,missing]
+
+print(findErrorNums([1,2,2,4]))
